f32465c4d993fde0550f60251c766d639b57d36a
Руслан <ru_gr@ukr.net>
2022-10-30 14:12:38 +0200

    fix title

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Лабраторна робота №1 \nГронюк Руслан Олександрович 2ПІ-22м (варіант №4");
+        primaryStage.setTitle("Лабраторна робота №1 \nГронюк Руслан Олександрович 2ПІ-22м (варіант №4)");
         Scene scene = new Scene(root, 1024, 768);
         primaryStage.setScene(scene);
         primaryStage.show();
